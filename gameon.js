1c7893e93e349f645ade093479bdc8562667b9a7
Raf Smits <raf_smits@hotmail.com>
2016-04-15 17:28:31 +0200

    Initialize GameOn

---
+++ gameon.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var match = 1; 
+	
+	hideShowMatch(match);
+	
+	$("#prev").click(function(e) {
+		e.preventDefault();
+		if (match == 1) {
+			return;
+		} else {
+			match -= 1;
+			hideShowMatch(match);
+		}
+	});
+	
+	$("#next").click(function(e) {
+		e.preventDefault();
+		if (match == $("#matches").children().length) {
+			return;
+		} else {
+			match += 1;
+			hideShowMatch(match);
+		}
+	});
+});
+
+function hideShowMatch(match) {
+	$("#matches").children().hide();
+	$("#match-" + match).show();
+}
